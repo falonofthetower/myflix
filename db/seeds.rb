ac64c187583b0ed961bf8094610907aa11e9fa54
Peter Karth <falonofthetower@gmail.com>
2015-04-13 20:02:45 -0400

    prep course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create name: "Comedies"
+Category.create name: "Dramas"
+Video.create title: "Monk", description: "Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", small_cover_url: "monk", large_cover_url: "monk_large", category_id: 1
+Video.create title: "Monk", description: "Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", small_cover_url: "monk", large_cover_url: "monk_large", category_id: 1
+Video.create title: "Monk", description: "Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", small_cover_url: "monk", large_cover_url: "monk_large", category_id: 1
+Video.create title: "Monk", description: "Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", small_cover_url: "monk", large_cover_url: "monk_large", category_id: 1
+Video.create title: "Family Guy", description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.", small_cover_url: "family_guy", large_cover_url: "family_guy_large", category_id: 2
+
+Video.create title: "South Park", description: "Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.", small_cover_url: "south_park", large_cover_url: "south_park_large", category_id: 2
+
+Video.create title: "South Park", description: "Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.", small_cover_url: "south_park", large_cover_url: "south_park_large", category_id: 2
+
+Video.create title: "Futurama", description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover_url: "futurama", large_cover_url: "futurama_large", category_id: 2
+
+Video.create title: "Futurama", description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover_url: "futurama", large_cover_url: "futurama_large", category_id: 2
+
+
