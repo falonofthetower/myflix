d9118e849f0ad81ba6710331a8ccfeed851b23ee
Peter Karth <falonofthetower@gmail.com>
2015-04-13 20:11:07 -0400

    narrowed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-  resources :videos
-  resources :categories
+  resources :videos, only: [:show, :index]
+
+  resources :categories, only: [:show]
 end
