c1d2b434ef7e2374c03d4d784206c62ded828c4a
Peter Karth <falonofthetower@gmail.com>
2015-04-19 13:23:13 -0400

    Update category_spec.rb
    adding in parens

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,7 +4,7 @@ describe Category do
   it "saves itself" do
     first_category = Category.create name: "The category of love"
     first_category.save
-    expect(Category.first).to eq first_category
+    expect(Category.first).to eq(first_category)
   end
 
   it { should have_many(:videos) }
