4266392023af6187f517c142bcab4c96713493ad
Peter Karth <falonofthetower@gmail.com>
2015-04-19 12:05:17 -0400

    testing basic validations and added shoulda

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,10 +7,5 @@ describe Category do
     expect(Category.first).to eq first_category
   end
 
-  it "has many videos" do
-    dramas = Category.create name: "drama"
-    monk = Video.create title: "Monk", description: "Dective show", category: dramas
-    south_park = Video.create title: "South Park", description: "Cartoon", category: dramas
-    expect(dramas.videos).to include monk, south_park
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,11 +7,9 @@ describe Video do
    expect(Video.first).to eq first_video
   end
 
-  it "belongs to category" do
-    dramas = Category.create name: "dramas"
-    monk = Video.create title: "Monk", description: "Dective show", category: dramas
-    expect(monk.category).to eq dramas
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
 
 
