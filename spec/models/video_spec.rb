fa6f5f400f5729e29e8c72a10ee2cc492353b4e2
Peter Karth <falonofthetower@gmail.com>
2015-04-19 13:22:35 -0400

    Update video_spec.rb
    adding in parens

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Video do
   it "saves itself" do
-   first_video = Video.create title: "The first title", description: "The first description"
+   first_video = Video.create(title: "The first title", description: "The first description")
    first_video.save
    expect(Video.first).to eq(first_video)
   end
