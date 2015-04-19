6c29d7de417102abca45bd63843602642e4ca533
Peter Karth <falonofthetower@gmail.com>
2015-04-19 13:23:37 -0400

    Update category_spec.rb
    adding in parens

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Category do
   it "saves itself" do
-    first_category = Category.create name: "The category of love"
+    first_category = Category.create(name: "The category of love")
     first_category.save
     expect(Category.first).to eq(first_category)
   end
