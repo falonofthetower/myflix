bf5ed994883258eb237cd1c19a32716313386f3a
Peter Karth <falonofthetower@gmail.com>
2015-04-19 13:22:06 -0400

    Update video_spec.rb
    Adding in parens

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,7 +4,7 @@ describe Video do
   it "saves itself" do
    first_video = Video.create title: "The first title", description: "The first description"
    first_video.save
-   expect(Video.first).to eq first_video
+   expect(Video.first).to eq(first_video)
   end
 
   it { should belong_to(:category) }
