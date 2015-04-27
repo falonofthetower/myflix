6dbec4224eec41ccf60d29b2ca207b965bc22506
Peter Karth <falonofthetower@gmail.com>
2015-04-27 11:27:34 -0400

    first review

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,16 @@ class SessionsController < ApplicationController
   def new
     redirect_to home_path if logged_in?
   end
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:info] = "You have logged in"
-      redirect_to videos_path
+      flash[:success] = "You have logged in"
+      redirect_to home_path
     else
       flash[:danger] = "Your credentials are invalid!"
-      redirect_to login_path
+      redirect_to sign_in_path
     end
   end
 
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,19 +2,41 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
-      context "with authenticated users" do
-        before 
-        session[:user_id] = Fabricate(:user).id
-      it "sets the @video variable" do
-        video = Fabricate(:video)
-
-        get :show, id: video.id
-        expect(assigns(:videos)).to eq(video)
-      end
+    it "sets the @video variable for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
     end
 
-    it "renders the show template" do
+    it "sets the @reviews for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      review_1 = Fabricate(:review, video: video)
+      review_2 = Fabricate(:review, video: video)
+      get :show, id: video.id
+      expect(assigns(:reviews)).to match_array([review_1, review_2])
+    end
 
+    it "redirects the unauthenticated user to the sign in page" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
+    end
+  end
+
+  describe "POST search" do
+    it "sets @results for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      futurama = Fabricate(:video, title: 'Futurama')
+      post :search, search_term: 'rama'
+      expect(assigns(:results)).to eq([futurama])
+    end
+
+    it "redirects unauthenticated users to sign_in_path" do
+      futurama = Fabricate(:video, title: 'Futurama')
+      post :search, search_term: 'rama'
+      expect(response).to redirect_to sign_in_path
     end
   end
 end
