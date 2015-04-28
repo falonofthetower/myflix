b1bda3da08659729cdfcebd91620eaba4d4355a7
Peter Karth <falonofthetower@gmail.com>
2015-04-28 11:41:29 -0400

    Update seeds.rb
    Updating formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,29 @@
 comedies = Category.create name: "Comedies"
 dramas = Category.create name: "Dramas"
 
-family_guy = Video.create title: "Family Guy", description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.", small_cover_url: "family_guy", large_cover_url: "family_guy_large", category: comedies
+family_guy = Video.create title: "Family Guy", 
+                          description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.", 
+                          small_cover_url: "family_guy", 
+                          large_cover_url: "family_guy_large", 
+                          category: comedies
 
-7.times { Video.create title: "Monk", description: "Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", small_cover_url: "monk", large_cover_url: "monk_large", category: dramas }
+7.times { Video.create title: "Monk", 
+                       description: "Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", 
+                       small_cover_url: "monk", 
+                       large_cover_url: "monk_large", 
+                       category: dramas }
 
-2.times { Video.create title: "South Park", description: "Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.", small_cover_url: "south_park", large_cover_url: "south_park_large", category: comedies }
+2.times { Video.create title: "South Park", 
+                       description: "Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.", 
+                       small_cover_url: "south_park", 
+                       large_cover_url: "south_park_large", 
+                       category: comedies }
 
-2.times { Video.create title: "Futurama", description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover_url: "futurama", large_cover_url: "futurama_large", category: comedies }
+2.times { Video.create title: "Futurama", 
+                       description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", 
+                       small_cover_url: "futurama", 
+                       large_cover_url: "futurama_large", 
+                       category: comedies }
 
 darth_vader = User.create(full_name: "James Earl Jones", password: "I am your father", email: "anakin@deathstar.com")
 
