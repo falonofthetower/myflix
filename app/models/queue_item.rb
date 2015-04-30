41b88ca67f4757237bd966993381180a35cb124c
Peter Karth <falonofthetower@gmail.com>
2015-04-30 10:39:19 -0400

    Update queue_item.rb
    Removed duplicative method

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -5,10 +5,6 @@ class QueueItem < ActiveRecord::Base
   delegate :category, to: :video
   delegate :title, to: :video, prefix: :video
 
-  def video_title
-    video.title
-  end
-
   def rating
     review = Review.where(user_id: user.id, video_id: video.id).first
     review.rating if review
