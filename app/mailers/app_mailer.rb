7008f37370cecbaa9f443cda45ae4ac67656ba70
Peter Karth <falonofthetower@gmail.com>
2015-06-01 01:37:15 -0400

    mailer from

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,6 +1,6 @@
 class AppMailer < ActionMailer::Base
   def welcome(user)
     @user = user
-    mail from: 'peter.karth.email@gmail.com', to: user.email, subject: "You are now a member of the MyFlix Family!"
+    mail from: 'info@myflix.com', to: user.email, subject: "You are now a member of the MyFlix Family!"
   end
 end
