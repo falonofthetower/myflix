7eb1e20e8f0666d82fbae5f18bb7d99d9287e9e6
Peter Karth <falonofthetower@gmail.com>
2015-06-06 00:00:21 -0400

    updated to: default in mailer

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,18 +1,18 @@
 class AppMailer < ActionMailer::Base
-  default from: 'info@myflix.com'
+  default from: 'info@peter-myflix.herokuapp.com'
 
   def welcome(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: "You are now a member of the MyFlix Family!"
+    mail to: user.email, subject: "You are now a member of the MyFlix Family!"
   end
 
   def send_forgot_password(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: "Please reset your password"
+    mail to: user.email, subject: "Please reset your password"
   end
 
   def send_invitation(invitation)
     @invitation = invitation
-    mail from: 'info@myflix.com', to: invitation.recipient_email, subject: "Inivitation to join MyFlix"
+    mail to: invitation.recipient_email, subject: "Inivitation to join MyFlix"
   end
 end
