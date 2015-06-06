8419ad50861de0848ba55e07687aeaddb367b97a
Peter Karth <falonofthetower@gmail.com>
2015-06-06 13:24:42 -0400

    even more alignment

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -75,19 +75,14 @@ describe UsersController do
     context "email sending" do
       it "sends out the email given valid inputs" do
         post :create, user: Fabricate.attributes_for(
-          :user,
-          email: "example@example.com",
-          full_name: "The Wizard of Id"
+          :user, email: "example@example.com", full_name: "The Wizard of Id"
         )
         expect(ActionMailer::Base.deliveries.last.to).to eq(["example@example.com"])
       end
 
       it "email sent out has users name" do
-        post :create,
-          user: Fabricate.attributes_for(
-            :user,
-            email: "example@example.com",
-            full_name: "The Wizard of Id"
+        post :create, user: Fabricate.attributes_for(
+          :user, email: "example@example.com", full_name: "The Wizard of Id"
         )
         expect(ActionMailer::Base.deliveries.last.body).to include("The Wizard of Id")
       end
@@ -98,7 +93,7 @@ describe UsersController do
           full_name: "The Wizard of Id",
           password: ""
         }
-          expect(ActionMailer::Base.deliveries).to be_empty
+        expect(ActionMailer::Base.deliveries).to be_empty
       end
     end
 
