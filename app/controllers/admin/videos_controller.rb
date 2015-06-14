36aec1a42dd41338bffb3998a224ac234a90674a
Peter Karth <falonofthetower@gmail.com>
2015-06-14 01:03:44 -0400

    upload images on videos

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,10 +6,25 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "#{@video.title} successfully saved!"
+      redirect_to new_admin_video_path
+    else
+      flash[:danger] = "Check error messages!"
+      render :new
+    end
+  end
+
   def require_admin
     if !current_user.admin?
       flash[:danger] = "You don't have authorization to do that"
       redirect_to home_path
     end
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :description, :small_cover, :large_cover, :category_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611171433) do
+ActiveRecord::Schema.define(version: 20150613222112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,11 +69,11 @@ ActiveRecord::Schema.define(version: 20150611171433) do
   create_table "videos", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "small_cover_url"
-    t.string   "large_cover_url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.string   "small_cover"
+    t.string   "large_cover"
   end
 
 end
